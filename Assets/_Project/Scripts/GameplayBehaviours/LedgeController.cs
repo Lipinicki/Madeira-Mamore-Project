3255cdf49b8987ffb6f65b90937a30c51beaf598
waagreen <silvacostawagner@gmail.com>
2023-03-18 21:30:57 -0300

    ladder climbing

--- Assets/_Project/Scripts/GameplayBehaviours/LedgeController.cs
+++ Assets/_Project/Scripts/GameplayBehaviours/LedgeController.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider))]
 public class LedgeController : MonoBehaviour
 {
-    [SerializeField] private Transform standPosition;
     [SerializeField] private Collider grabDetection;
-
-    public Vector3 GetStandPosition()
-    {
-        return standPosition.position;
-    }
+    
 }
