4a5f4d2a5a1b3bb2e61d9b27e20f510dfbc1e1b9
Agostinho Felipe <agostinhofelipeneto@outlook.com>
2023-05-23 19:28:23 -0300

    PlayTest Build

--- Assets/_Project/Scripts/Player Interactions/InteractBehaviour/InteractableDoorLockBehaviour.cs
+++ Assets/_Project/Scripts/Player Interactions/InteractBehaviour/InteractableDoorLockBehaviour.cs
@@ -1,29 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using MyBox;
-using DG.Tweening;
 
 public class InteractableDoorLockBehaviour : MonoBehaviour, IInteractable
 {
     [SerializeField] private bool useLever = false; // if true the door will open using a lever instead of a key
     [ConditionalField(nameof(useLever), inverse: true)][SerializeField] private Inventory playerInventory;
     [ConditionalField(nameof(useLever), inverse: true)][SerializeField] private string keyItemName;
-    [SerializeField] private Animator animator;
-    [SerializeField] private UnityEvent OnDoorOpenned;
 
-    private readonly int r_DoorAnimationState = Animator.StringToHash("DoorOpening");
+    [Space(10), Header("Aniamtions")]
+    [SerializeField] private Animator animator;
+    [SerializeField] private string doorAnimationName;
+
+    [Space(10)]
+    [SerializeField] private UnityEvent OnDoorOpenned;
 
 	void IInteractable.Interact()
 	{
         if (playerInventory?.GetItem(keyItemName) == null && !useLever)
         {
+#if UNITY_EDITOR
             Debug.LogWarning("Item: " + keyItemName + "not found");
+#endif
             return;
         }
 
-        animator.Play(r_DoorAnimationState);
+        animator.Play(Animator.StringToHash(doorAnimationName));
         OnDoorOpenned?.Invoke();
         this.enabled = false;
 	}
