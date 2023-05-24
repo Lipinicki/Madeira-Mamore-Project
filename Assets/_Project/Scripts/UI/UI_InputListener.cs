a363089e33e135898aaa1548e14e138114d247fa
Agostinho Felipe <agostinhofelipeneto@outlook.com>
2023-05-24 11:28:14 -0300

    Corrigido return do inventário com nenhum item

--- Assets/_Project/Scripts/UI/UI_InputListener.cs
+++ Assets/_Project/Scripts/UI/UI_InputListener.cs
@@ -1,25 +1,34 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-public class UI_CreditsInput : MonoBehaviour
+// Listens for input directly from player's input events
+public class UI_InputListener : MonoBehaviour
 {
     [SerializeField] private PlayerInput playerInput;
 
 	[Space(10)]
 	[SerializeField] private UnityEvent MenuCancel;
-
+	[SerializeField] private UnityEvent SubmitEvent;
+ 
 	private void OnEnable()
 	{
 		playerInput.menusCancelEvent += OnMenuCancel;
+		playerInput.menusSubmitEvent += OnSubmit;
 	}
 
 	private void OnDisable()
 	{
 		playerInput.menusCancelEvent -= OnMenuCancel;
+		playerInput.menusSubmitEvent -= OnSubmit;
 	}
 
 	private void OnMenuCancel()
 	{
 		MenuCancel?.Invoke();
+	}
+
+	private void OnSubmit()
+	{
+		SubmitEvent?.Invoke();
 	}
 }
